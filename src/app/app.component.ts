919234b67be42070b9f038f0a39e523d05ef051c
varun-kumar06 <kumarmvarun39@gmail.com>
2024-10-05 16:39:34 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReactForm';
+  myForm = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.maxLength(5)]),
+    address: new FormGroup({  
+      doorNo: new FormControl("", [Validators.required]), 
+      street: new FormControl("", [Validators.maxLength(10)]),
+    }),
+  });
+
+  get name(){
+    return this.myForm.get("name");
+  }
+
+  
+  get doorNo() {
+    return this.myForm.get("doorNo");
+  }
+
+  submit(){
+    console.log(this.myForm);
+  }
 }
